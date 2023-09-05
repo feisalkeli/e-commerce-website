5378a7c753cad12b256ba0b94dec28eda6b54feb
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-09-05 22:22:20 +0300

    Install dependancies

---
+++ e-commerce/src/components/Products/Products.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Grid } from "@mui/material";
+
+const Products = () => {
+  return (
+    <main>
+      <Grid conatiner justify="center" spacing={4}></Grid>
+    </main>
+  );
+};
+
+export default Products;
